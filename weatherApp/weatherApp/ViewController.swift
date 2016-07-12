5200c9c103652a1e70eaf9f0dc858220385e6eed
LeTchang <mvallet@student.42.fr>
2016-07-12 13:36:08 +0200

    add exceptions in Info.plist

--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
-    let Paris = City(name: "Paris", country: "FR", id: 6455259, long: 2.35236, lat: 48.856461)
-
+    let Paris = City(name: "Paris", country: "FR", id: 2988507, long: 2.35236, lat: 48.856461)
+    let key = "8a25d66f07d77f5b66bdf01bb8f25dda"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = "api.openweathermap.org/data/2.5/forecast?id=" + String(Paris.id)
+        initData()
+    }
+    
+    func initData() {
+        let url = "http://api.openweathermap.org/data/2.5/forecast?id=" + String(Paris.id) + "&APPID=" + key
         print(url)
+        Alamofire.request(.POST, url).validate().responseJSON {
+            response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    print(json.description)
+                }
+            case .Failure:
+                print("Error while loading datas")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
