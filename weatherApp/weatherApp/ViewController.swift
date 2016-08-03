5d41a5eddeedf7f82beac638e18346e2a4e1404a
LeTchang <mvallet@student.42.fr>
2016-08-03 08:26:03 +0200

    split some code

--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -45,47 +45,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: - Check and download all the data needed and save it
     func updateData(completion: () -> Void) {
         let url = "http://api.openweathermap.org/data/2.5/forecast?id=" + String(Paris.id) + "&APPID=" + key
-        let prevision = Meteo()
-        var currentDay:String? = nil
-        var dayNumber = 0
-        var first = true
-        prevision.created = NSDate()
-        prevision.meteo.append(Day())
         Alamofire.request(.POST, url).validate().responseJSON {
             response in
             switch response.result {
             case .Success:
                 if let value = response.result.value {
                     let json = JSON(value)
-                    let n = json["list"].count
-                    for x in 0 ..< n {
-                        let newWeather = Weather()
-                        let date = json["list"][x]["dt_txt"].string!
-                        let day = date.componentsSeparatedByString(" ")[0]
-                        let time = date.componentsSeparatedByString(" ")[1]
-                        newWeather.icon = json["list"][x]["weather"][0]["icon"].string!
-                        newWeather.id = String(json["list"][x]["weather"][0]["id"])
-                        newWeather.main = json["list"][x]["weather"][0]["main"].string!
-                        newWeather.descri = json["list"][x]["weather"][0]["description"].string!
-                        newWeather.time = time
-                        newWeather.tMax = (json["list"][x]["main"]["temp_max"].float?.celcius)!
-                        newWeather.tMin = (json["list"][x]["main"]["temp_min"].float?.celcius)!
-                        newWeather.t = (json["list"][x]["main"]["temp"].float?.celcius)!
-                        if (currentDay != day) {
-                            currentDay = day
-                            if first {
-                                first = false
-                            } else {
-                                prevision.meteo.append(Day())
-                                dayNumber += 1
-                            }
-                            prevision.meteo[dayNumber].date = day
-                        }
-                        prevision.meteo[dayNumber].weathers.append(newWeather)
-                    }
-                    try! myRealm.write {
-                        myRealm.add(prevision)
-                    }
+                    self.downloadData(json)
                 completion()
                 }
             case .Failure:
@@ -96,6 +62,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 alert.view.setNeedsLayout()
                 self.presentViewController(alert, animated: true, completion: nil)
             }
+        }
+    }
+    
+    func downloadData(json: JSON) {
+        let prevision = Meteo()
+        var currentDay:String? = nil
+        var dayNumber = 0
+        var first = true
+        let n = json["list"].count
+        prevision.created = NSDate()
+        prevision.meteo.append(Day())
+        
+        for x in 0 ..< n {
+            let newWeather = Weather()
+            let date = json["list"][x]["dt_txt"].string!
+            let day = date.componentsSeparatedByString(" ")[0]
+            let time = date.componentsSeparatedByString(" ")[1]
+            newWeather.icon = json["list"][x]["weather"][0]["icon"].string!
+            newWeather.id = String(json["list"][x]["weather"][0]["id"])
+            newWeather.main = json["list"][x]["weather"][0]["main"].string!
+            newWeather.descri = json["list"][x]["weather"][0]["description"].string!
+            newWeather.time = time
+            newWeather.tMax = (json["list"][x]["main"]["temp_max"].float?.celcius)!
+            newWeather.tMin = (json["list"][x]["main"]["temp_min"].float?.celcius)!
+            newWeather.t = (json["list"][x]["main"]["temp"].float?.celcius)!
+            if (currentDay != day) {
+                currentDay = day
+                if first {
+                    first = false
+                } else {
+                    prevision.meteo.append(Day())
+                    dayNumber += 1
+                }
+                prevision.meteo[dayNumber].date = day
+            }
+            prevision.meteo[dayNumber].weathers.append(newWeather)
+        }
+        try! myRealm.write {
+            myRealm.add(prevision)
         }
     }
     
